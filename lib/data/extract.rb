49af93203cecbd3dd6914ec422388da1f841a201
mmhan <mmhan2u@gmail.com>
2015-06-13 17:10:28 +0630

    Reorganized files and added state ISO

--- lib/data/extract.rb
+++ lib/data/extract.rb
@@ -6,14 +6,14 @@ states = []
 districts = []
 townships = []
 
-CSV.foreach("locations.csv") do |s_pcode, state, d_pcode, district, t_pcode, township|
-	states << {pcode: s_pcode, name: state}
+CSV.foreach("locations.csv") do |iso, s_pcode, state, d_pcode, district, t_pcode, township|
+	states << {iso: iso, pcode: s_pcode, name: state}
 	districts << {pcode: d_pcode, name: district, state: s_pcode} unless d_pcode.nil?
 	townships << {pcode: t_pcode, name: township, distrct: d_pcode} unless t_pcode.nil?
 end
 states.uniq! { |state| state[:pcode] }
 districts.uniq! { |districts| districts[:pcode] }
 
-File.open("data/states.yml", "w") { |f| f.write states.to_yaml }
-File.open("data/districts.yml", "w") { |f| f.write districts.to_yaml }
-File.open("data/townships.yml", "w") { |f| f.write townships.to_yaml }
+File.open("states.yml", "w") { |f| f.write states.to_yaml }
+File.open("districts.yml", "w") { |f| f.write districts.to_yaml }
+File.open("townships.yml", "w") { |f| f.write townships.to_yaml }
