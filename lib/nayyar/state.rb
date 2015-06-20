814092f27d4c8cdb6cf79fb20d06fd423c0b56a7
mmhan <mmhan2u@gmail.com>
2015-06-20 18:24:52 +0630

    Done refactoring finder methods

--- lib/nayyar/state.rb
+++ lib/nayyar/state.rb
@@ -1,6 +1,11 @@
 class Nayyar::State
 	attr_reader :data
 
+  @@states = nil
+  @@pcode_index = []
+  @@iso_index = []
+  @@alpha3_index = []
+
 	attributes = [
 		:pcode,
 		:iso,
@@ -20,5 +25,75 @@ class Nayyar::State
 	def [](key)
 		@data[key]
 	end
-end
 
+	class << self
+	  INDICES = %w(pcode iso alpha3)
+
+		def all
+			states
+		end
+
+	  def find_by(query)
+	    key = get_key(query)
+	    (index = send("#{key}_index".to_sym).index(query[key])) && states[index]
+	  end
+
+	  def find_by!(query)
+	    if state = find_by(query)
+	      state
+	    else
+	      key = get_key(query)
+	      raise Nayyar::StateNotFound.new("Cannot find State with given #{key}: #{query[key]}")
+	    end
+	  end
+
+	  INDICES.each do |index|
+	  	define_method("find_by_#{index}") do |query|
+	  		find_by(index.to_sym => query)
+	  	end
+	  	define_method("find_by_#{index}!") do |query|
+	  		find_by!(index.to_sym => query)
+	  	end
+	  end
+
+
+	  protected
+	    def states
+	    	unless @@states
+	    		data = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'data', 'states.yml'))
+	    		@@states = data.map do |state|
+	    			state = new(state)
+	    			@@pcode_index << state.pcode
+	          @@iso_index << state.iso
+	          @@alpha3_index << state.alpha3
+	    			state
+	    		end
+	    	end
+	    	@@states
+	    end
+
+	    def pcode_index
+	    	states
+	    	@@pcode_index
+	    end
+
+	    def iso_index
+	    	states
+	    	@@iso_index
+	    end
+
+	    def alpha3_index
+	    	states
+	    	@@alpha3_index
+	    end
+
+		  def get_key(data)
+		    keys = data.keys
+		    if keys.length != 1 || %w(pcode iso alpha3).none? { |key| key.to_sym == keys.first }
+		      raise ArgumentError.new("`find_by` accepts only one of pcode, iso or alpha3 as argument. None provided")
+		    end
+		    keys.first
+		  end
+	end
+end
+class Nayyar::StateNotFound < StandardError; end;
--- spec/nayyar_spec.rb
+++ spec/nayyar_spec.rb
@@ -5,91 +5,70 @@ describe Nayyar do
   it 'has a version number' do
     expect(Nayyar::VERSION).not_to be nil
   end
-
-	describe ".states" do
-		subject { described_class.states }
-		it { is_expected.to be_an Array }
-		it "contains hashes of state data" do
-			expect(subject.first).to be_a Nayyar::State
-			expect(subject.size).to be > 1
-		end
-	end
-
-	describe ".state_with" do
-		subject { described_class.state_with(pcode: "MMR001") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will return nil if state is not found" do
-			expect(described_class.state_with(pcode: "FOOBAR")).to be_nil
-		end
-		it "will raise ArgumentError if no argument is provided" do
-			expect{described_class.state_with}.to raise_error ArgumentError
-		end
-	end
-
-	describe ".state_with!" do
-		subject { described_class.state_with!(pcode: "MMR001") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will raise error if the state isn't found" do
-			expect{ described_class.state_with!(pcode: "FOOBAR")}.to raise_error
-		end
-	end
-
-	describe ".state_with_pcode" do
-		subject { described_class.state_with_pcode("MMR001") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will return nil if state is not found" do
-			expect(described_class.state_with_pcode("FOOBAR")).to be_nil
-		end
-	end
-	describe ".state_with_pcode!" do
-		subject { described_class.state_with_pcode!("MMR001") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will raise error if state is not found" do
-			expect {described_class.state_with_pcode!("FOOBAR")}.to raise_error Nayyar::StateNotFound
-		end
-	end
-
-	describe ".state_with_iso" do
-		subject { described_class.state_with_iso("MM-01") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will return nil if state is not found" do
-			expect(described_class.state_with_iso("MM-00")).to be_nil
-		end
-	end
-	describe ".state_with_iso!" do
-		subject { described_class.state_with_iso!("MM-01") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will raise error if state is not found" do
-			expect {described_class.state_with_iso!("MM-00")}.to raise_error Nayyar::StateNotFound
-		end
-	end
-
-	describe ".state_with_alpha3" do
-		subject { described_class.state_with_alpha3("YGN") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will return nil if state is not found" do
-			expect(described_class.state_with_alpha3("ABC")).to be_nil
-		end
-	end
-	describe ".state_with_alpha3!" do
-		subject { described_class.state_with_alpha3!("YGN") }
-		it { is_expected.to be_a Nayyar::State }
-		it "will raise error if state is not found" do
-			expect {described_class.state_with_alpha3!("ABC")}.to raise_error Nayyar::StateNotFound
-		end
-	end
-
-
-	describe ".get_key" do
-		subject { described_class.get_key(iso: "") }
-		it { is_expected.to eq(:iso) }
-		it "will raise_error if key is not in whitelist" do
-			expect{ described_class.get_key(foo: "bar") }.to raise_error ArgumentError
-		end
-	end
 end
 
 describe Nayyar::State do
+
+	describe "class methods" do
+
+		let(:valid_queries) { %w(pcode iso alpha3).zip(%w(MMR001 MM-01 YGN)).to_h }
+		let(:invalid_queries) { %w(pcode iso alpha3).zip(%w(FOOBAR MM-00 ABC)).to_h }
+
+		describe ".all" do
+			subject { described_class.all }
+			it { is_expected.to be_an Array }
+			it "contains hashes of state data" do
+				expect(subject.first).to be_a Nayyar::State
+				expect(subject.size).to be > 1
+			end
+		end
+
+		describe ".find_by" do
+			subject { described_class.find_by(pcode: "MMR001") }
+			it { is_expected.to be_a Nayyar::State }
+			it "will return nil if state is not found" do
+				expect(described_class.find_by(pcode: "FOOBAR")).to be_nil
+			end
+			it "will raise ArgumentError if no argument is provided" do
+				expect{described_class.find_by}.to raise_error ArgumentError
+			end
+		end
+
+		describe ".find_by!" do
+			subject { described_class.find_by!(pcode: "MMR001") }
+			it { is_expected.to be_a Nayyar::State }
+			it "will raise error if the state isn't found" do
+				expect{ described_class.find_by!(pcode: "FOOBAR")}.to raise_error
+			end
+		end
+
+		describe ".find_by_**indices**" do
+			it "returns state when it finds it" do
+				valid_queries.each do |index, query|
+					expect(described_class.send("find_by_#{index}", query)).to be_a Nayyar::State
+				end
+			end
+			it "returns nil when it can't be found" do
+				invalid_queries.each do |index, query|
+					expect(described_class.send("find_by_#{index}", query)).to be_nil
+				end
+			end
+		end
+
+		describe ".find_by_**indices**!" do
+			it "returns state when it finds it" do
+				valid_queries.each do |index, query|
+					expect(described_class.send("find_by_#{index}!", query)).to be_a Nayyar::State
+				end
+			end
+			it "raise error when it can't be found" do
+				invalid_queries.each do |index, query|
+					expect{ described_class.send("find_by_#{index}!", query) }.to raise_error
+				end
+			end
+		end
+	end
+
 	let(:state) do
 		described_class.new( pcode: "MMR000",
 			name: "Fake State",
